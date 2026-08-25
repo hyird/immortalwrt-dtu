f97efc5dfcdae935518627a5cae97cbf388c9398
hyird <hyird@users.noreply.github.com>
2026-08-25 16:22:07 +0800

    ramips: remove target apk from TAS-682F image

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -9,7 +9,7 @@ define Device/tastek_it692
   DEVICE_VENDOR := Tastek
   DEVICE_MODEL := TAS-682F
   DEVICE_VARIANT := IT692 / SUMMIT DTU IoT
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci \
+  DEVICE_PACKAGES := -apk-openssl kmod-usb2 kmod-usb-ohci \
 	kmod-usb-net-rndis kmod-usb-serial-option
   SUPPORTED_DEVICES += tastek,it692 tas-682
 endef
