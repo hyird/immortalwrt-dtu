19811429ef372d313c47f681ff41796de35c8479
hyird <hyird@users.noreply.github.com>
2026-09-03 10:08:00 +0800

    ramips: add Tastek IT-694_s3 support

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -16,3 +16,16 @@ define Device/tastek_it692
   SUPPORTED_DEVICES += tas-682
 endef
 TARGET_DEVICES += tastek_it692
+
+define Device/tastek_it694
+  IMAGE_SIZE := 7872k
+  DEVICE_VENDOR := Tastek
+  DEVICE_MODEL := IT-694_s3
+  DEVICE_VARIANT := MT7628AN / Wi-Fi / 4G DTU
+  DEVICE_PACKAGES := -apk-openssl -apk-mbedtls -opkg \
+	kmod-mt7603 kmod-usb2 kmod-usb-ohci \
+	kmod-usb-net-rndis kmod-usb-serial-option
+  # tastek,it694 is supplied by the image framework from DEVICE_NAME.
+  SUPPORTED_DEVICES += it-694-s3
+endef
+TARGET_DEVICES += tastek_it694
