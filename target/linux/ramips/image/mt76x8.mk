864ff0b803852da67086a1de5935a771df22971d
hyird <hyird@users.noreply.github.com>
2026-09-04 15:35:32 +0800

    fix(it694): include wpad in device image

--- target/linux/ramips/image/mt76x8.mk
+++ target/linux/ramips/image/mt76x8.mk
@@ -23,7 +23,7 @@ define Device/tastek_it694
   DEVICE_MODEL := IT-694_s3
   DEVICE_VARIANT := MT7628AN / Wi-Fi / 4G DTU
   DEVICE_PACKAGES := -apk-openssl -apk-mbedtls -opkg \
-	kmod-mt7603 kmod-usb2 kmod-usb-ohci \
+	kmod-mt7603 wpad-basic-mbedtls kmod-usb2 kmod-usb-ohci \
 	kmod-usb-net-rndis kmod-usb-serial-option
   # tastek,it694 is supplied by the image framework from DEVICE_NAME.
   SUPPORTED_DEVICES += it-694-s3
